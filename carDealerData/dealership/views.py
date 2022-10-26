2f4e83d82fdaffcfc2ba08fcf38cc057792d3a63
jwilki23 <wilkinsonjacob22@gmail.com>
2022-10-26 16:34:05 -0600

    Made views

--- carDealerData/dealership/views.py
+++ carDealerData/dealership/views.py
@@ -1,3 +1,13 @@
+from curses.ascii import HT
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from http.client import HTTPResponse
 # Create your views here.
+def indexPageView(request) :
+    return HttpResponse('This is our homepage')
+
+def aboutPageView(request) :
+    return HttpResponse('This is our about page')
+
+def contactPageView(request) :
+    return HttpResponse('This is our contact page')
