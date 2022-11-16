eb34150adbd246767facfe1cda60116365486cb7
bsmiley2000 <bsmiley2000@gmail.com>
2022-11-16 15:14:10 -0700

    fdc

--- dealership/admin.py
+++ dealership/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Customer, TripCategory, Destination
 
-
+#python3 manage.py createsuperuser
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(TripCategory)
