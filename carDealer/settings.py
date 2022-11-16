f681095ad34dad5ce13369ba34337bc5ead8aa51
bsmiley2000 <bsmiley2000@gmail.com>
2022-11-16 14:36:57 -0700

    started adding models

--- carDealer/settings.py
+++ carDealer/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'carDealer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dealership',
+        'USER' : 'postgres',
+        'PASSWORD' : 'kindlefire',
+        'HOST' : 'localhost'
     }
 }
 
