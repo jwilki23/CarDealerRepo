cf3f4ee803c348ae9ee291c3fe105d6d8cc99baf
jwilki23 <wilkinsonjacob22@gmail.com>
2022-11-17 18:40:53 -0700

    took out my password

--- carDealer/settings.py
+++ carDealer/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dealership',
         'USER' : 'postgres',
-        'PASSWORD' : 'Wannabe22!',
+        'PASSWORD' : '',
         'HOST' : 'localhost'
     }
 }
