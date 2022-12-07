06b988b257858afa02e0133c63424241c914911c
jwilki23 <wilkinsonjacob22@gmail.com>
2022-12-07 15:37:58 -0700

    Add database

--- carDealer/settings.py
+++ carDealer/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,16 +80,21 @@ WSGI_APPLICATION = 'carDealer.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dealership',
-        'USER' : 'postgres',
-        'PASSWORD' : '',
-        'HOST' : 'localhost'
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'dealership',
+#         'USER' : 'postgres',
+#         'PASSWORD' : '',
+#         'HOST' : 'localhost'
+#     }
+# }
 
 
 # Password validation
